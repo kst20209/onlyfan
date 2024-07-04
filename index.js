7e82ff70cb04e0240f07035570b3e5b3b1508cc5
Zeropepsi123 <kst20209@gmail.com>
2024-07-04 23:12:27 +0900

    .

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -9,10 +8,23 @@ const wss = new WebSocket.Server({ server });
 let globalFanState = "정지";
 let lastUpdatedBy = "";
 
-wss.on('connection', (ws) => {
+const ipRequestTimes = {};
+
+wss.on('connection', (ws, req) => {
+    const ip = req.socket.remoteAddress;
+
     ws.send(JSON.stringify({ fanState: globalFanState, updatedBy: lastUpdatedBy }));
 
     ws.on('message', (message) => {
+        const now = Date.now();
+
+        if (ipRequestTimes[ip] && (now - ipRequestTimes[ip] < 100)) {
+            ws.send(JSON.stringify({ error: '너무 잦은 요청' }));
+            return;
+        }
+
+        ipRequestTimes[ip] = now;
+
         const data = JSON.parse(message);
         globalFanState = data.fanState;
         lastUpdatedBy = data.userName;

d69316d6c30faf595b26d88f6e248ec1a2e45324
Zeropepsi123 <kst20209@gmail.com>
2024-07-04 23:42:20 +0900

    PORT fix

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ wss.on('connection', (ws, req) => {
 
 app.use(express.static('public'));
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
